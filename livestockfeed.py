1fb832bff673f50ad790f79a0a769ae88fbd70eb
Dev_elio <32529318+devpatelio@users.noreply.github.com>
2019-12-06 12:11:53 -0500

    Updated file with graph and time.

--- livestockfeed.py
+++ livestockfeed.py
@@ -1,3 +1,5 @@
+~~~~~ Refresh File has live data overwrite
+
 
 from first_sequence import*
 
@@ -5,11 +7,30 @@ from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
+import time
 
 ts = TimeSeries(key='E7W0A9REUL6LY9J9', output_format='pandas')
 
-data, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')
-print(data)
+data, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')  #replace symbol with the variable stock after importing first_sequence
+
+# time
+
+live_set = []
+
+def livedata():
+    live, meta_data = ts.get_intraday(symbol=stock, interval='1min', outputsize='full')
+    # live_set.append(live['4. close'])
+    closingprice = live['4. close']
+    live_set.append(closingprice[5])
+    print(live_set)  # remove from statement when you will create email functionality
+
+
+
+while True:
+    livedata()
+    time.sleep(60)
+
+
 
 fig = plt.figure(figsize=(12, 8))
 style.use('fivethirtyeight')
